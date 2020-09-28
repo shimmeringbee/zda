3b62692293e1d189c8e24705bbfdd781c629135a
Peter Wood <peter@alastria.net>
2020-09-28 08:39:25 +0100

    Add initial reimplementation of HasProductInformation capability.

--- capability/impl/has_product_information/implementation.go
+++ capability/impl/has_product_information/implementation.go
@@ -2,107 +2,152 @@ package has_product_information
 
 import (
 	"context"
-	"fmt"
-	"github.com/shimmeringbee/callbacks"
 	"github.com/shimmeringbee/da"
 	"github.com/shimmeringbee/da/capabilities"
-	"github.com/shimmeringbee/retry"
 	"github.com/shimmeringbee/zcl"
-	"github.com/shimmeringbee/zigbee"
-	"log"
+	"github.com/shimmeringbee/zda"
+	"github.com/shimmeringbee/zda/capability"
 )
 
-type ZigbeeHasProductInformation struct {
-	gateway               da.Gateway
-	nodeTable             nodeTable
-	internalCallbacks     callbacks.Adder
-	zclGlobalCommunicator zclGlobalCommunicator
-	capabilityManager     CapabilityManager
+type ProductData struct {
+	Manufacturer *string
+	Product      *string
 }
 
-func (z *ZigbeeHasProductInformation) Capability() da.Capability {
+type Implementation struct {
+	stopCh chan struct{}
+	msgCh  chan interface{}
+	data   map[zda.IEEEAddressWithSubIdentifier]ProductData
+
+	supervisor capability.Supervisor
+}
+
+func (i *Implementation) Capability() da.Capability {
 	return capabilities.HasProductInformationFlag
 }
 
-func (z *ZigbeeHasProductInformation) Init() {
-	z.internalCallbacks.Add(z.NodeEnumerationCallback)
+func (i *Implementation) Init(supervisor capability.Supervisor) {
+	i.stopCh = make(chan struct{})
+	i.msgCh = make(chan interface{})
+	i.data = make(map[zda.IEEEAddressWithSubIdentifier]ProductData)
+	i.supervisor = supervisor
+
+	supervisor.EventSubscription().AddedDevice(i.addedDeviceCallback)
+	supervisor.EventSubscription().RemovedDevice(i.removedDeviceCallback)
+	supervisor.EventSubscription().EnumerateDevice(i.enumerateDeviceCallback)
 }
 
-func (z *ZigbeeHasProductInformation) NodeEnumerationCallback(ctx context.Context, ide internalDeviceEnumeration) error {
-	iDev := ide.device
-	iNode := iDev.node
+func (i *Implementation) Start() {
+	go i.loop()
+}
 
-	iDev.mutex.Lock()
+func (i *Implementation) Stop() {
+	i.stopCh <- struct{}{}
+}
 
-	found := false
-	var foundEndpoint zigbee.Endpoint
-
-	for _, endpoint := range iDev.endpoints {
-		if isClusterIdInSlice(iNode.endpointDescriptions[endpoint].InClusterList, zcl.BasicId) {
-			found = true
-			foundEndpoint = endpoint
-			break
+func (i *Implementation) loop() {
+	for {
+		select {
+		case <-i.stopCh:
+			return
+		case m := <-i.msgCh:
+			i.handle(m)
 		}
 	}
+}
 
-	if found {
-		if err := retry.Retry(ctx, DefaultNetworkTimeout, DefaultNetworkRetries, func(ctx context.Context) error {
-			readRecords, err := z.zclGlobalCommunicator.ReadAttributes(ctx, iNode.ieeeAddress, iNode.supportsAPSAck, zcl.BasicId, zigbee.NoManufacturer, DefaultGatewayHomeAutomationEndpoint, foundEndpoint, iNode.nextTransactionSequence(), []zcl.AttributeID{0x0004, 0x0005})
+func (i *Implementation) handle(m interface{}) {
+	switch e := m.(type) {
+	default:
+	case addedDeviceReq:
+		i.handleAddedDeviceRequest(e)
+	case removedDeviceReq:
+		i.handleRemovedDeviceRequest(e)
+	case productInformationReq:
+		i.handleProductInformationReq(e)
+	case enumerateDeviceReq:
+		i.handleEnumerateDeviceReq(e)
+	case enumerateDeviceComplete:
+		i.handleEnumerateDeviceComplete(e)
+	}
+}
 
-			if err == nil {
-				for _, record := range readRecords {
-					switch record.Identifier {
-					case 0x0004:
-						if record.Status == 0 {
-							iDev.productInformation.Manufacturer = record.DataTypeValue.Value.(string)
-							iDev.productInformation.Present |= capabilities.Manufacturer
-						} else {
-							iDev.productInformation.Manufacturer = ""
-							iDev.productInformation.Present &= ^capabilities.Manufacturer
-						}
+func (i *Implementation) handleAddedDeviceRequest(e addedDeviceReq) {
+	if _, found := i.data[e.device.Identifier]; !found {
+		i.data[e.device.Identifier] = ProductData{}
+	}
 
-					case 0x0005:
-						if record.Status == 0 {
-							iDev.productInformation.Name = record.DataTypeValue.Value.(string)
-							iDev.productInformation.Present |= capabilities.Name
-						} else {
-							iDev.productInformation.Name = ""
-							iDev.productInformation.Present &= ^capabilities.Name
-						}
-					}
-				}
+	e.ch <- nil
+}
+
+func (i *Implementation) handleRemovedDeviceRequest(e removedDeviceReq) {
+	delete(i.data, e.device.Identifier)
+
+	e.ch <- nil
+}
+
+func (i *Implementation) handleProductInformationReq(e productInformationReq) {
+	var resp productInformationResp
+
+	if data, found := i.data[e.device.Identifier]; found {
+		if data.Manufacturer != nil {
+			resp.ProductInformation.Manufacturer = *data.Manufacturer
+			resp.ProductInformation.Present |= capabilities.Manufacturer
+		}
+
+		if data.Product != nil {
+			resp.ProductInformation.Name = *data.Product
+			resp.ProductInformation.Present |= capabilities.Name
+		}
+
+	} else {
+		resp.error = da.DeviceDoesNotBelongToGatewayError
+	}
+
+	e.ch <- resp
+}
+
+func (i *Implementation) handleEnumerateDeviceReq(e enumerateDeviceReq) {
+	endpoints := capability.FindEndpointsWithClusterID(e.device, zcl.BasicId)
+
+	if len(endpoints) == 0 {
+		i.data[e.device.Identifier] = ProductData{}
+		i.supervisor.ManageDeviceCapabilities().Remove(e.device, capabilities.HasProductInformationFlag)
+		e.ch <- nil
+	} else {
+		endpoint := endpoints[0]
+
+		go func() {
+			ctx, cancel := context.WithTimeout(context.Background(), zda.DefaultNetworkTimeout*zda.DefaultNetworkRetries)
+			defer cancel()
+
+			var productData ProductData
+
+			records, err := i.supervisor.ZCL().ReadAttributes(ctx, e.device, endpoint, zcl.BasicId, []zcl.AttributeID{0x0004, 0x0005})
+			if err != nil {
+				e.ch <- err
+				return
 			}
 
-			return err
-		}); err != nil {
-			log.Printf("failed to read product information: %s", err)
-		}
+			if records[0x0004].Status == 0 {
+				productData.Manufacturer = records[0x0004].DataTypeValue.Value.(*string)
+			}
 
-		iDev.mutex.Unlock()
-		z.capabilityManager.AddCapabilityToDevice(iDev.generateIdentifier(), capabilities.HasProductInformationFlag)
-	} else {
-		iDev.mutex.Unlock()
+			if records[0x0005].Status == 0 {
+				productData.Product = records[0x0005].DataTypeValue.Value.(*string)
+			}
+
+			i.msgCh <- enumerateDeviceComplete{
+				device:      e.device,
+				productData: productData,
+				ch:          e.ch,
+			}
+		}()
 	}
-
-	return nil
 }
 
-func (z *ZigbeeHasProductInformation) ProductInformation(ctx context.Context, device da.Device) (capabilities.ProductInformation, error) {
-	if da.DeviceDoesNotBelongToGateway(z.gateway, device) {
-		return capabilities.ProductInformation{}, da.DeviceDoesNotBelongToGatewayError
-	}
-
-	if !device.HasCapability(capabilities.HasProductInformationFlag) {
-		return capabilities.ProductInformation{}, da.DeviceDoesNotHaveCapability
-	}
-
-	iDev := z.nodeTable.getDevice(device.Identifier().(IEEEAddressWithSubIdentifier))
-	if iDev != nil {
-		iDev.mutex.RLock()
-		defer iDev.mutex.RUnlock()
-
-		return iDev.productInformation, nil
-	} else {
-		return capabilities.ProductInformation{}, fmt.Errorf("internal node table error, missing device")
-	}
+func (i *Implementation) handleEnumerateDeviceComplete(e enumerateDeviceComplete) {
+	i.supervisor.ManageDeviceCapabilities().Add(e.device, capabilities.HasProductInformationFlag)
+	i.data[e.device.Identifier] = e.productData
+	e.ch <- nil
 }
--- capability/impl/has_product_information/implementation_test.go
+++ capability/impl/has_product_information/implementation_test.go
@@ -1,1 +1,318 @@
 package has_product_information
+
+import (
+	"github.com/shimmeringbee/da"
+	"github.com/shimmeringbee/da/capabilities"
+	"github.com/shimmeringbee/zcl"
+	"github.com/shimmeringbee/zcl/commands/global"
+	"github.com/shimmeringbee/zda"
+	"github.com/shimmeringbee/zda/capability"
+	"github.com/shimmeringbee/zda/capability/mocks"
+	"github.com/shimmeringbee/zigbee"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestImplementation_Capability(t *testing.T) {
+	t.Run("matches the CapabiltiyBasic interface and returns the correct Capability", func(t *testing.T) {
+		impl := &Implementation{}
+
+		assert.Implements(t, (*capability.BasicCapability)(nil), impl)
+		assert.Equal(t, capabilities.HasProductInformationFlag, impl.Capability())
+	})
+}
+
+func TestImplementation_InitableCapability(t *testing.T) {
+	t.Run("matches the InitableCapability interface", func(t *testing.T) {
+		impl := &Implementation{}
+
+		assert.Implements(t, (*capability.InitableCapability)(nil), impl)
+	})
+}
+
+func TestImplementation_ProcessingCapability(t *testing.T) {
+	t.Run("matches the ProcessingCapability interface", func(t *testing.T) {
+		impl := &Implementation{}
+
+		assert.Implements(t, (*capability.ProcessingCapability)(nil), impl)
+	})
+}
+
+func TestImplementation_Init(t *testing.T) {
+	t.Run("subscribes to events", func(t *testing.T) {
+		impl := &Implementation{}
+
+		mockSupervisor := &mocks.MockSupervisor{}
+		mockEventSubscription := &mocks.MockEventSubscription{}
+
+		mockSupervisor.On("EventSubscription").Return(mockEventSubscription)
+
+		mockEventSubscription.On("AddedDevice", mock.Anything)
+		mockEventSubscription.On("RemovedDevice", mock.Anything)
+		mockEventSubscription.On("EnumerateDevice", mock.Anything)
+		defer mockEventSubscription.AssertExpectations(t)
+
+		impl.Init(mockSupervisor)
+	})
+}
+
+func TestImplementation_handleAddedDeviceRequest(t *testing.T) {
+	t.Run("adding a device is added to the store, and a nil is returned on the channel", func(t *testing.T) {
+		impl := &Implementation{}
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{}
+
+		replyCh := make(chan error, 1)
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		impl.handleAddedDeviceRequest(addedDeviceReq{
+			device: capability.Device{
+				Identifier: addr,
+			},
+			ch: replyCh,
+		})
+
+		assert.Len(t, replyCh, 1)
+		assert.Contains(t, impl.data, addr)
+	})
+}
+
+func TestImplementation_handleRemovedDeviceRequest(t *testing.T) {
+	t.Run("removing a device is removed from the store, and a nil is returned on the channel", func(t *testing.T) {
+		impl := &Implementation{}
+
+		replyCh := make(chan error, 1)
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{
+			addr: {},
+		}
+
+		impl.handleRemovedDeviceRequest(removedDeviceReq{
+			device: capability.Device{
+				Identifier: addr,
+			},
+			ch: replyCh,
+		})
+
+		assert.Len(t, replyCh, 1)
+		assert.NotContains(t, impl.data, addr)
+	})
+}
+
+func TestImplementation_handleProductInformationReq(t *testing.T) {
+	t.Run("returns a product information with both fields", func(t *testing.T) {
+		impl := &Implementation{}
+
+		replyCh := make(chan productInformationResp, 1)
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		expectedManufacturer := "manu"
+		expectedProduct := "name"
+
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{
+			addr: {
+				Manufacturer: &expectedManufacturer,
+				Product:      &expectedProduct,
+			},
+		}
+
+		impl.handleProductInformationReq(productInformationReq{
+			device: capability.Device{
+				Identifier: addr,
+			},
+			ch: replyCh,
+		})
+
+		assert.Len(t, replyCh, 1)
+
+		if len(replyCh) > 0 {
+			msg := <-replyCh
+
+			assert.Equal(t, expectedManufacturer, msg.ProductInformation.Manufacturer)
+			assert.Equal(t, expectedProduct, msg.ProductInformation.Name)
+			assert.Equal(t, capabilities.Manufacturer|capabilities.Name, msg.ProductInformation.Present)
+		}
+	})
+}
+
+func TestImplementation_handleEnumerateDeviceReq(t *testing.T) {
+	t.Run("removes capability if no endpoints have the Basic cluster", func(t *testing.T) {
+		impl := &Implementation{}
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{}
+
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		device := capability.Device{
+			Identifier:   addr,
+			Capabilities: []da.Capability{},
+			Endpoints: map[zigbee.Endpoint]zigbee.EndpointDescription{
+				0x00: {
+					Endpoint:      0x00,
+					InClusterList: []zigbee.ClusterID{},
+				},
+			},
+		}
+
+		existingManu := "existingManu"
+		existingProduct := "existingProduct"
+
+		mockManageDeviceCapabilities := mocks.MockManageDeviceCapabilities{}
+		defer mockManageDeviceCapabilities.AssertExpectations(t)
+
+		mockManageDeviceCapabilities.On("Remove", device, capabilities.HasProductInformationFlag)
+
+		impl.supervisor = &capability.SimpleSupervisor{
+			MDCImpl: &mockManageDeviceCapabilities,
+		}
+
+		impl.data[addr] = ProductData{
+			Manufacturer: &existingManu,
+			Product:      &existingProduct,
+		}
+
+		ch := make(chan error, 1)
+
+		impl.handleEnumerateDeviceReq(enumerateDeviceReq{
+			device: device,
+			ch:     ch,
+		})
+
+		assert.Len(t, ch, 1)
+
+		assert.Nil(t, impl.data[addr].Manufacturer)
+		assert.Nil(t, impl.data[addr].Product)
+	})
+
+	t.Run("adds capability and sets product data if on first endpoint that has Basic cluster", func(t *testing.T) {
+		impl := &Implementation{}
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{}
+
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		device := capability.Device{
+			Identifier:   addr,
+			Capabilities: []da.Capability{},
+			Endpoints: map[zigbee.Endpoint]zigbee.EndpointDescription{
+				0x00: {
+					Endpoint:      0x00,
+					InClusterList: []zigbee.ClusterID{zcl.BasicId},
+				},
+			},
+		}
+
+		mockManageDeviceCapabilities := mocks.MockManageDeviceCapabilities{}
+		mockZCL := mocks.MockZCL{}
+
+		defer mockManageDeviceCapabilities.AssertExpectations(t)
+		defer mockZCL.AssertExpectations(t)
+
+		expectedManufacturer := "manu"
+		expectedProduct := "product"
+
+		mockZCL.On("ReadAttributes", mock.Anything, device, zigbee.Endpoint(0x00), zcl.BasicId, []zcl.AttributeID{0x0004, 0x0005}).
+			Return(map[zcl.AttributeID]global.ReadAttributeResponseRecord{
+				0x0004: {
+					Status: 0x00,
+					DataTypeValue: &zcl.AttributeDataTypeValue{
+						DataType: zcl.TypeStringCharacter8,
+						Value:    &expectedManufacturer,
+					},
+				},
+				0x0005: {
+					Status: 0x00,
+					DataTypeValue: &zcl.AttributeDataTypeValue{
+						DataType: zcl.TypeStringCharacter8,
+						Value:    &expectedProduct,
+					},
+				},
+			}, nil)
+
+		impl.supervisor = &capability.SimpleSupervisor{
+			MDCImpl: &mockManageDeviceCapabilities,
+			ZCLImpl: &mockZCL,
+		}
+
+		impl.data[addr] = ProductData{}
+
+		impl.msgCh = make(chan interface{}, 1)
+		ch := make(chan error)
+
+		impl.handleEnumerateDeviceReq(enumerateDeviceReq{
+			device: device,
+			ch:     ch,
+		})
+
+		msg := <-impl.msgCh
+		enumCompMsg := msg.(enumerateDeviceComplete)
+
+		assert.Equal(t, ch, enumCompMsg.ch)
+		assert.Equal(t, device, enumCompMsg.device)
+		assert.Equal(t, expectedManufacturer, *enumCompMsg.productData.Manufacturer)
+		assert.Equal(t, expectedProduct, *enumCompMsg.productData.Product)
+	})
+
+	t.Run("adds capability and sets product data if on first endpoint that has Basic cluster", func(t *testing.T) {
+		impl := &Implementation{}
+		impl.data = map[zda.IEEEAddressWithSubIdentifier]ProductData{}
+
+		addr := zda.IEEEAddressWithSubIdentifier{
+			IEEEAddress:   zigbee.GenerateLocalAdministeredIEEEAddress(),
+			SubIdentifier: 0x01,
+		}
+
+		device := capability.Device{
+			Identifier:   addr,
+			Capabilities: []da.Capability{},
+			Endpoints: map[zigbee.Endpoint]zigbee.EndpointDescription{
+				0x00: {
+					Endpoint:      0x00,
+					InClusterList: []zigbee.ClusterID{zcl.BasicId},
+				},
+			},
+		}
+
+		mockManageDeviceCapabilities := mocks.MockManageDeviceCapabilities{}
+
+		defer mockManageDeviceCapabilities.AssertExpectations(t)
+
+		expectedManufacturer := "manu"
+		expectedProduct := "product"
+
+		mockManageDeviceCapabilities.On("Add", device, capabilities.HasProductInformationFlag)
+
+		impl.supervisor = &capability.SimpleSupervisor{
+			MDCImpl: &mockManageDeviceCapabilities,
+		}
+
+		impl.data[addr] = ProductData{}
+
+		ch := make(chan error, 1)
+
+		impl.handleEnumerateDeviceComplete(enumerateDeviceComplete{
+			device:      device,
+			productData: ProductData{Manufacturer: &expectedManufacturer, Product: &expectedProduct},
+			ch:          ch,
+		})
+
+		<-ch
+
+		assert.Equal(t, expectedManufacturer, *impl.data[addr].Manufacturer)
+		assert.Equal(t, expectedProduct, *impl.data[addr].Product)
+	})
+}

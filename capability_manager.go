739ed619e566b29fa52b5290df1b7ee1a74bd8dc
Peter Wood <peter@alastria.net>
2020-09-29 22:02:00 +0100

    Add base of capability manager, acting as a store for all capabilities.

---
+++ capability_manager.go
@@ -0,0 +1,55 @@
+package zda
+
+import "github.com/shimmeringbee/da"
+
+func NewCapabilityManager() *CapabilityManager {
+	return &CapabilityManager{
+		capabilityByFlag:    map[da.Capability]interface{}{},
+		capabilityByKeyName: map[string]interface{}{},
+	}
+}
+
+type CapabilityManager struct {
+	capabilityByFlag    map[da.Capability]interface{}
+	capabilityByKeyName map[string]interface{}
+}
+
+func (m *CapabilityManager) Add(c BasicCapability) {
+	m.capabilityByFlag[c.Capability()] = c
+
+	if pc, ok := c.(PersistableCapability); ok {
+		m.capabilityByKeyName[pc.KeyName()] = pc
+	}
+}
+
+func (m *CapabilityManager) Get(c da.Capability) interface{} {
+	return m.capabilityByFlag[c]
+}
+
+func (m *CapabilityManager) PersistingCapabilities() map[string]interface{} {
+	return m.capabilityByKeyName
+}
+
+func (m *CapabilityManager) Init() {
+	for _, capability := range m.capabilityByFlag {
+		if c, ok := capability.(InitableCapability); ok {
+			c.Init(nil)
+		}
+	}
+}
+
+func (m *CapabilityManager) Start() {
+	for _, capability := range m.capabilityByFlag {
+		if c, ok := capability.(ProcessingCapability); ok {
+			c.Start()
+		}
+	}
+}
+
+func (m *CapabilityManager) Stop() {
+	for _, capability := range m.capabilityByFlag {
+		if c, ok := capability.(ProcessingCapability); ok {
+			c.Stop()
+		}
+	}
+}

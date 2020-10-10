d868d1964d3a9e11ab66d27eb48f93c4695e7087
Peter Wood <peter@alastria.net>
2020-10-10 12:46:38 +0100

    Add support for Relative Humidity Measurement, supports #3.

---
+++ capability/relative_humidity_sensor/main.go
@@ -0,0 +1,70 @@
+package relative_humidity_sensor
+
+import (
+	"context"
+	"github.com/shimmeringbee/da"
+	"github.com/shimmeringbee/da/capabilities"
+	"github.com/shimmeringbee/zcl"
+	"github.com/shimmeringbee/zcl/commands/global"
+	"github.com/shimmeringbee/zcl/commands/local/relative_humidity_measurement"
+	"github.com/shimmeringbee/zda"
+	"github.com/shimmeringbee/zigbee"
+	"sync"
+)
+
+type Data struct {
+	State           float64
+	RequiresPolling bool
+	PollerCancel    func()
+	Endpoint        zigbee.Endpoint
+}
+
+type PersistentData struct {
+	State           float64
+	RequiresPolling bool
+	Endpoint        zigbee.Endpoint
+}
+
+type Implementation struct {
+	supervisor zda.CapabilitySupervisor
+
+	data     map[zda.IEEEAddressWithSubIdentifier]Data
+	datalock *sync.RWMutex
+}
+
+func (i *Implementation) Capability() da.Capability {
+	return capabilities.RelativeHumiditySensorFlag
+}
+
+func (i *Implementation) Init(supervisor zda.CapabilitySupervisor) {
+	i.supervisor = supervisor
+
+	i.data = map[zda.IEEEAddressWithSubIdentifier]Data{}
+	i.datalock = &sync.RWMutex{}
+
+	i.supervisor.ZCL().Listen(func(address zigbee.IEEEAddress, appMsg zigbee.ApplicationMessage, zclMessage zcl.Message) bool {
+		_, canCast := zclMessage.Command.(*global.ReportAttributes)
+		return zclMessage.ClusterID == zcl.RelativeHumidityMeasurementId && canCast
+	}, i.zclCallback)
+}
+
+func (i *Implementation) pollDevice(ctx context.Context, d zda.Device) bool {
+	i.datalock.RLock()
+	data, found := i.data[d.Identifier]
+	i.datalock.RUnlock()
+
+	if !found {
+		return false
+	}
+
+	endpoint := data.Endpoint
+
+	results, err := i.supervisor.ZCL().ReadAttributes(ctx, d, endpoint, zcl.RelativeHumidityMeasurementId, []zcl.AttributeID{relative_humidity_measurement.MeasuredValue})
+	if err == nil {
+		if results[relative_humidity_measurement.MeasuredValue].Status == 0 {
+			i.setState(d, results[relative_humidity_measurement.MeasuredValue].DataTypeValue.Value.(uint64))
+		}
+	}
+
+	return true
+}
---
+++ capability/relative_humidity_sensor/persistence.go
@@ -0,0 +1,67 @@
+package relative_humidity_sensor
+
+import (
+	"fmt"
+	"github.com/shimmeringbee/da"
+	"github.com/shimmeringbee/da/capabilities"
+	"github.com/shimmeringbee/zda"
+)
+
+const PersistenceName = "RelativeHumiditySensor"
+
+func (i *Implementation) KeyName() string {
+	return PersistenceName
+}
+
+func (i *Implementation) DataStruct() interface{} {
+	return &PersistentData{}
+}
+
+func (i *Implementation) Save(d zda.Device) (interface{}, error) {
+	if !d.HasCapability(capabilities.RelativeHumiditySensorFlag) {
+		return nil, da.DeviceDoesNotHaveCapability
+	}
+
+	i.datalock.RLock()
+	defer i.datalock.RUnlock()
+
+	return &PersistentData{
+		State:           i.data[d.Identifier].State,
+		RequiresPolling: i.data[d.Identifier].RequiresPolling,
+		Endpoint:        i.data[d.Identifier].Endpoint,
+	}, nil
+}
+
+func (i *Implementation) Load(d zda.Device, state interface{}) error {
+	if !d.HasCapability(capabilities.RelativeHumiditySensorFlag) {
+		return da.DeviceDoesNotHaveCapability
+	}
+
+	pd, ok := state.(*PersistentData)
+	if !ok {
+		return fmt.Errorf("invalid data structure provided for load")
+	}
+
+	i.datalock.Lock()
+	defer i.datalock.Unlock()
+
+	if i.data[d.Identifier].PollerCancel != nil {
+		i.data[d.Identifier].PollerCancel()
+	}
+
+	var pollerCancelFn func()
+
+	if pd.RequiresPolling {
+		cfg := i.supervisor.DeviceConfig().Get(d, capabilities.StandardNames[capabilities.RelativeHumiditySensorFlag])
+		pollerCancelFn = i.supervisor.Poller().Add(d, cfg.Duration("PollingInterval", DefaultPollingInterval), i.pollDevice)
+	}
+
+	i.data[d.Identifier] = Data{
+		State:           pd.State,
+		RequiresPolling: pd.RequiresPolling,
+		PollerCancel:    pollerCancelFn,
+		Endpoint:        pd.Endpoint,
+	}
+
+	return nil
+}

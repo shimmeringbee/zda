cf6a25be68d1a8956d045f0eaf3dabe72c6efeee
Peter Wood <peter@alastria.net>
2020-10-30 17:59:42 +0000

    Add support for returning enumeration status.

--- internal_node.go
+++ internal_node.go
@@ -17,6 +17,7 @@ type internalNode struct {
 	endpoints            []zigbee.Endpoint
 	endpointDescriptions map[zigbee.Endpoint]zigbee.EndpointDescription
 
+	enumerating          bool
 	transactionSequences chan uint8
 	supportsAPSAck       bool
 }

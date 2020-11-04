bd89041a62d51877be7b541cb199a22aba76b127
Peter Wood <peter@alastria.net>
2020-11-04 20:41:45 +0000

    Add logging to HasProductInformation.

--- capability/has_product_information/callbacks.go
+++ capability/has_product_information/callbacks.go
@@ -3,6 +3,7 @@ package has_product_information
 import (
 	"context"
 	"github.com/shimmeringbee/da/capabilities"
+	"github.com/shimmeringbee/logwrap"
 	"github.com/shimmeringbee/zcl"
 	"github.com/shimmeringbee/zcl/commands/local/basic"
 	"github.com/shimmeringbee/zda"
@@ -40,21 +41,27 @@ func (i *Implementation) EnumerateDevice(ctx context.Context, d zda.Device) erro
 	} else {
 		endpoint := endpoints[0]
 
+		i.supervisor.Logger().LogInfo(ctx, "Have Product Information capability.", logwrap.Datum("Endpoint", endpoint))
+
 		var productData ProductData
 
+		i.supervisor.Logger().LogDebug(ctx, "Querying Basic cluster for name and model.")
 		records, err := i.supervisor.ZCL().ReadAttributes(ctx, d, endpoint, zcl.BasicId, []zcl.AttributeID{basic.ManufacturerName, basic.ModelIdentifier})
 		if err != nil {
+			i.supervisor.Logger().LogError(ctx, "Failed to query Basic cluster for details", logwrap.Err(err))
 			return err
 		}
 
 		if records[basic.ManufacturerName].Status == 0 {
 			manufacturerString := records[basic.ManufacturerName].DataTypeValue.Value.(string)
 			productData.Manufacturer = &manufacturerString
+			i.supervisor.Logger().LogInfo(ctx, "Manufacturer name retrieved.", logwrap.Datum("Name", manufacturerString))
 		}
 
 		if records[basic.ModelIdentifier].Status == 0 {
 			productString := records[basic.ModelIdentifier].DataTypeValue.Value.(string)
 			productData.Product = &productString
+			i.supervisor.Logger().LogInfo(ctx, "Product name retrieved.", logwrap.Datum("Name", productString))
 		}
 
 		i.datalock.Lock()

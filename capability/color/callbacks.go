3b93582df3495e0d5582676d1e9e68a19378bf18
Peter Wood <peter@alastria.net>
2020-12-22 19:58:23 +0000

    Add enumeration and attribute monitoring for color.

--- capability/color/callbacks.go
+++ capability/color/callbacks.go
@@ -62,13 +62,30 @@ func (i *Implementation) EnumerateDevice(ctx context.Context, d zda.Device) erro
 			i.supervisor.Logger().LogError(ctx, "Failed to read color capabilities from color control cluster.", logwrap.Err(err))
 		}
 
-		data.SupportsHueSat = cfg.Bool("SupportsHueSat", results[color_control.ColorCapabilities].Status == 0 && results[color_control.ColorCapabilities].DataTypeValue.Value.(uint64)&0b00000001 > 0)
-		data.SupportsXY = cfg.Bool("SupportsXY", results[color_control.ColorCapabilities].Status == 0 && results[color_control.ColorCapabilities].DataTypeValue.Value.(uint64)&0b00001000 > 0)
-		data.SupportsTemperature = cfg.Bool("SupportsTemperature", results[color_control.ColorCapabilities].Status == 0 && results[color_control.ColorCapabilities].DataTypeValue.Value.(uint64)&0b00010000 > 0)
+		colorCapabilities := uint8(0)
+
+		if results[color_control.ColorCapabilities].Status == 0 {
+			colorCapabilities = uint8(results[color_control.ColorCapabilities].DataTypeValue.Value.(uint64))
+		}
+
+		data.SupportsHueSat = cfg.Bool("SupportsHueSat", colorCapabilities&0b00000001 > 0)
+		data.SupportsXY = cfg.Bool("SupportsXY", colorCapabilities&0b00001000 > 0)
+		data.SupportsTemperature = cfg.Bool("SupportsTemperature", colorCapabilities&0b00010000 > 0)
+
+		hasCapability = data.SupportsXY || data.SupportsHueSat || data.SupportsTemperature
+
+		if !hasCapability {
+			i.supervisor.Logger().LogError(ctx, "Device has cluster, but ColorCapability attribute reports no suitable color spaces.", logwrap.Datum("Endpoint", data.Endpoint), logwrap.Datum("ColorCapabilities", colorCapabilities))
+		}
 	}
 
 	if !hasCapability {
-		//i.attributeMonitor.Detach(ctx, d)
+		i.attMonColorMode.Detach(ctx, d)
+		i.attMonCurrentHue.Detach(ctx, d)
+		i.attMonCurrentSat.Detach(ctx, d)
+		i.attMonCurrentX.Detach(ctx, d)
+		i.attMonCurrentY.Detach(ctx, d)
+		i.attMonCurrentTemp.Detach(ctx, d)
 
 		i.datalock.Lock()
 		i.data[d.Identifier] = data
@@ -78,16 +95,58 @@ func (i *Implementation) EnumerateDevice(ctx context.Context, d zda.Device) erro
 	} else {
 		i.supervisor.Logger().LogInfo(ctx, "Have Color capability.", logwrap.Datum("Endpoint", data.Endpoint), logwrap.Datum("SupportsHueSat", data.SupportsHueSat), logwrap.Datum("SupportsXY", data.SupportsXY), logwrap.Datum("SupportsTemperature", data.SupportsTemperature))
 
-		if data.SupportsXY {
+		if requiresPolling, err := i.attMonColorMode.Attach(ctx, d, data.Endpoint, nil); err != nil {
+			i.supervisor.Logger().LogError(ctx, "Failed to attach Color Mode attribute monitor to device.", logwrap.Err(err))
+			return err
+		} else {
+			i.supervisor.Logger().LogDebug(ctx, "Attached Color Mode attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+			data.RequiresPolling = requiresPolling
+		}
 
+		if data.SupportsXY {
+			if requiresPolling, err := i.attMonCurrentX.Attach(ctx, d, data.Endpoint, nil); err != nil {
+				i.supervisor.Logger().LogError(ctx, "Failed to attach CurrentX attribute monitor to device.", logwrap.Err(err))
+				return err
+			} else {
+				i.supervisor.Logger().LogDebug(ctx, "Attached CurrentX attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+				data.RequiresPolling = requiresPolling
+			}
+
+			if requiresPolling, err := i.attMonCurrentY.Attach(ctx, d, data.Endpoint, nil); err != nil {
+				i.supervisor.Logger().LogError(ctx, "Failed to attach CurrentY attribute monitor to device.", logwrap.Err(err))
+				return err
+			} else {
+				i.supervisor.Logger().LogDebug(ctx, "Attached CurrentY attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+				data.RequiresPolling = requiresPolling
+			}
 		}
 
 		if data.SupportsHueSat {
+			if requiresPolling, err := i.attMonCurrentHue.Attach(ctx, d, data.Endpoint, nil); err != nil {
+				i.supervisor.Logger().LogError(ctx, "Failed to attach Current Hue attribute monitor to device.", logwrap.Err(err))
+				return err
+			} else {
+				i.supervisor.Logger().LogDebug(ctx, "Attached Current Hue attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+				data.RequiresPolling = requiresPolling
+			}
 
+			if requiresPolling, err := i.attMonCurrentSat.Attach(ctx, d, data.Endpoint, nil); err != nil {
+				i.supervisor.Logger().LogError(ctx, "Failed to attach Current Sat attribute monitor to device.", logwrap.Err(err))
+				return err
+			} else {
+				i.supervisor.Logger().LogDebug(ctx, "Attached Current Sat attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+				data.RequiresPolling = requiresPolling
+			}
 		}
 
 		if data.SupportsTemperature {
-
+			if requiresPolling, err := i.attMonCurrentTemp.Attach(ctx, d, data.Endpoint, nil); err != nil {
+				i.supervisor.Logger().LogError(ctx, "Failed to attach Current Temperature attribute monitor to device.", logwrap.Err(err))
+				return err
+			} else {
+				i.supervisor.Logger().LogDebug(ctx, "Attached Current Temperature attribute monitor.", logwrap.Datum("RequiresPolling", requiresPolling))
+				data.RequiresPolling = requiresPolling
+			}
 		}
 
 		i.datalock.Lock()

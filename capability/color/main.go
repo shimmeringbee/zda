c87f3ddddec38bd0c21498a23cefb1ba999d0af8
Peter Wood <peter@alastria.net>
2020-12-22 22:59:30 +0000

    Add handling of updating state from attribute monitors, and sending events regarding new data.

--- capability/color/main.go
+++ capability/color/main.go
@@ -3,7 +3,7 @@ package color
 import (
 	"github.com/shimmeringbee/da"
 	"github.com/shimmeringbee/da/capabilities"
-	"github.com/shimmeringbee/da/capabilities/color"
+	da_color "github.com/shimmeringbee/da/capabilities/color"
 	"github.com/shimmeringbee/zcl"
 	"github.com/shimmeringbee/zcl/commands/local/color_control"
 	"github.com/shimmeringbee/zda"
@@ -12,8 +12,14 @@ import (
 )
 
 type State struct {
-	CurrentMode        capabilities.Mode
-	CurrentColor       color.ConvertibleColor
+	CurrentMode uint8
+
+	CurrentX float64
+	CurrentY float64
+
+	CurrentHue float64
+	CurrentSat float64
+
 	CurrentTemperature float64
 }
 
@@ -27,25 +33,15 @@ type Data struct {
 	SupportsTemperature bool
 }
 
-type PersistentColor struct {
-	ColorSpace color.NativeColorspace
-
-	X  float64 `json:",omitempty"`
-	Y  float64 `json:",omitempty"`
-	Y2 float64 `json:",omitempty"`
-
-	Hue   float64 `json:",omitempty"`
-	Sat   float64 `json:",omitempty"`
-	Value float64 `json:",omitempty"`
-
-	R uint8 `json:",omitempty"`
-	G uint8 `json:",omitempty"`
-	B uint8 `json:",omitempty"`
-}
-
 type PersistentState struct {
-	CurrentMode        capabilities.Mode
-	CurrentColor       PersistentColor
+	CurrentMode uint8
+
+	CurrentX float64
+	CurrentY float64
+
+	CurrentHue float64
+	CurrentSat float64
+
 	CurrentTemperature float64
 }
 
@@ -89,13 +85,92 @@ func (i *Implementation) Init(supervisor zda.CapabilitySupervisor) {
 
 	i.supervisor.ZCL().RegisterCommandLibrary(color_control.Register)
 
-	i.attMonColorMode = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.ColorMode, zcl.TypeUnsignedInt8, nil)
+	i.attMonColorMode = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.ColorMode, zcl.TypeUnsignedInt8, i.attributeUpdate)
 
-	i.attMonCurrentX = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentX, zcl.TypeUnsignedInt16, nil)
-	i.attMonCurrentY = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentY, zcl.TypeUnsignedInt16, nil)
+	i.attMonCurrentX = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentX, zcl.TypeUnsignedInt16, i.attributeUpdate)
+	i.attMonCurrentY = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentY, zcl.TypeUnsignedInt16, i.attributeUpdate)
 
-	i.attMonCurrentHue = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentHue, zcl.TypeUnsignedInt8, nil)
-	i.attMonCurrentSat = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentSaturation, zcl.TypeUnsignedInt8, nil)
+	i.attMonCurrentHue = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentHue, zcl.TypeUnsignedInt8, i.attributeUpdate)
+	i.attMonCurrentSat = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.CurrentSaturation, zcl.TypeUnsignedInt8, i.attributeUpdate)
 
-	i.attMonCurrentTemp = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.ColorTemperatureMireds, zcl.TypeUnsignedInt16, nil)
+	i.attMonCurrentTemp = i.supervisor.AttributeMonitorCreator().Create(i, zcl.ColorControlId, color_control.ColorTemperatureMireds, zcl.TypeUnsignedInt16, i.attributeUpdate)
+}
+
+func (i *Implementation) attributeUpdate(d zda.Device, a zcl.AttributeID, v zcl.AttributeDataTypeValue) {
+	i.datalock.Lock()
+	defer i.datalock.Unlock()
+
+	oldData := i.data[d.Identifier]
+	newData := i.data[d.Identifier]
+
+	if v.DataType == zcl.TypeUnsignedInt8 {
+		data := uint8(v.Value.(uint64))
+
+		switch a {
+		case color_control.ColorMode:
+			newData.State.CurrentMode = data
+		case color_control.CurrentHue:
+			newData.State.CurrentHue = float64(data) * 360.0 / 254.0
+		case color_control.CurrentSaturation:
+			newData.State.CurrentSat = float64(data) / 254.0
+		}
+	}
+
+	if v.DataType == zcl.TypeUnsignedInt16 {
+		data := uint16(v.Value.(uint64))
+
+		switch a {
+		case color_control.CurrentX:
+			newData.State.CurrentX = float64(data) / 65536.0
+		case color_control.CurrentY:
+			newData.State.CurrentY = float64(data) / 65536.0
+		case color_control.ColorTemperatureMireds:
+			newData.State.CurrentTemperature = 1000000 / float64(data)
+		}
+	}
+
+	if oldData.State != newData.State {
+		i.data[d.Identifier] = newData
+
+		i.supervisor.DAEventSender().Send(capabilities.ColorStatusUpdate{
+			Device: i.supervisor.ComposeDADevice().Compose(d),
+			State:  i.stateToColorStatus(newData.State),
+		})
+	}
+}
+
+func (i *Implementation) stateToColorStatus(state State) capabilities.ColorStatus {
+	var mode capabilities.Mode
+	var color da_color.ConvertibleColor
+	var temperature float64
+
+	switch state.CurrentMode {
+	case 0x00:
+		mode = capabilities.ColorMode
+		color = da_color.HSVColor{
+			Hue:   state.CurrentHue,
+			Sat:   state.CurrentSat,
+			Value: 1.0,
+		}
+	case 0x01:
+		mode = capabilities.ColorMode
+		color = da_color.XYColor{
+			X:  state.CurrentX,
+			Y:  state.CurrentY,
+			Y2: 100.0,
+		}
+	case 0x02:
+		mode = capabilities.TemperatureMode
+		temperature = state.CurrentTemperature
+	}
+
+	return capabilities.ColorStatus{
+		Mode: mode,
+		Color: capabilities.ColorSettings{
+			Current: color,
+		},
+		Temperature: capabilities.TemperatureSettings{
+			Current: temperature,
+		},
+	}
 }

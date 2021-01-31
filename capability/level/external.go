39a170bb01e58e3021fbbad0c0e72a5d5fb277da
Peter Wood <peter@alastria.net>
2021-01-31 14:46:48 +0000

    Fix copy and paste issues with last update/changed.

--- capability/level/external.go
+++ capability/level/external.go
@@ -61,7 +61,7 @@ func (i *Implementation) LastChangeTime(ctx context.Context, dad da.Device) (tim
 	d, found := i.supervisor.DeviceLookup().ByDA(dad)
 	if !found {
 		return time.Time{}, da.DeviceDoesNotBelongToGatewayError
-	} else if !d.HasCapability(capabilities.TemperatureSensorFlag) {
+	} else if !d.HasCapability(capabilities.LevelFlag) {
 		return time.Time{}, da.DeviceDoesNotHaveCapability
 	}
 
@@ -75,7 +75,7 @@ func (i *Implementation) LastUpdateTime(ctx context.Context, dad da.Device) (tim
 	d, found := i.supervisor.DeviceLookup().ByDA(dad)
 	if !found {
 		return time.Time{}, da.DeviceDoesNotBelongToGatewayError
-	} else if !d.HasCapability(capabilities.TemperatureSensorFlag) {
+	} else if !d.HasCapability(capabilities.LevelFlag) {
 		return time.Time{}, da.DeviceDoesNotHaveCapability
 	}
 

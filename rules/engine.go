ca2bb4ef5df4cf56d2608171387b0a45a5a621f4
Peter Wood <peter@alastria.net>
2022-10-11 22:02:01 +0100

    Resolution of on disk rules to compiled rules, including dependency resolution.

--- rules/engine.go
+++ rules/engine.go
@@ -1,11 +1,18 @@
 package rules
 
 import (
+	"fmt"
+	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/vm"
 	"io"
 	"io/fs"
+	"strings"
 )
 
-type Engine struct{}
+type Engine struct {
+	RuleSets map[string]RuleSet
+	Rules    []CompiledRule
+}
 
 type Capabilities struct {
 	Add    map[string]interface{}
@@ -21,6 +28,19 @@ type Rule struct {
 	Filter      string
 	Actions     Actions
 	Children    []Rule
+}
+
+type CompiledRule struct {
+	Description string
+	Filter      *vm.Program
+	Actions     Actions
+	Children    []CompiledRule
+}
+
+type RuleSet struct {
+	Name      string
+	DependsOn []string
+	Rules     []Rule
 }
 
 type InputProductData struct {
@@ -53,18 +73,93 @@ type Output struct {
 	Capabilities map[string]interface{}
 }
 
-func (e Engine) LoadString(_ string) error {
+func (e *Engine) LoadString(_ string) error {
 	panic("not yet implemented")
 }
 
-func (e Engine) LoadReader(_ io.Reader) error {
+func (e *Engine) LoadReader(_ io.Reader) error {
 	panic("not yet implemented")
 }
 
-func (e Engine) LoadFS(_ fs.FS) error {
+func (e *Engine) LoadFS(_ fs.FS) error {
 	panic("not yet implemented")
 }
 
-func (e Engine) Execute(_ Input) (Output, error) {
+func (e *Engine) CompileRules() error {
+	alreadyLoaded := map[string]bool{}
+
+	for k := range e.RuleSets {
+		alreadyLoaded[k] = false
+	}
+
+	for k := range e.RuleSets {
+		if !alreadyLoaded[k] {
+			if err := e.compileRuleSet(alreadyLoaded, []string{}, k); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (e *Engine) compileRuleSet(alreadyLoaded map[string]bool, trail []string, name string) error {
+	rs, ok := e.RuleSets[name]
+	if !ok {
+		return fmt.Errorf("ruleset missing dependency: %s->%s", strings.Join(trail, "->"), name)
+	}
+
+	trail = append(trail, rs.Name)
+
+	for _, k := range rs.DependsOn {
+		for _, t := range trail {
+			if k == t {
+				return fmt.Errorf("ruleset circular dependency: %s->%s", strings.Join(trail, "->"), k)
+			}
+		}
+
+		if !alreadyLoaded[k] {
+			if err := e.compileRuleSet(alreadyLoaded, trail, k); err != nil {
+				return err
+			}
+		}
+	}
+
+	if cr, err := compileRules(rs.Rules); err != nil {
+		return fmt.Errorf("ruleset compilation: %s: %w", strings.Join(trail, "->"), err)
+	} else {
+		e.Rules = append(e.Rules, cr...)
+	}
+
+	alreadyLoaded[name] = true
+
+	return nil
+}
+
+func compileRules(rules []Rule) ([]CompiledRule, error) {
+	var compiledRules []CompiledRule
+
+	for _, rule := range rules {
+		cf, err := expr.Compile(rule.Filter, expr.Env(Input{}))
+		if err != nil {
+			return nil, fmt.Errorf("filter compilation: %w", err)
+		}
+
+		if childCompiledRules, err := compileRules(rule.Children); err != nil {
+			return nil, fmt.Errorf("%s: %w", rule.Description, err)
+		} else {
+			compiledRules = append(compiledRules, CompiledRule{
+				Description: rule.Description,
+				Filter:      cf,
+				Actions:     rule.Actions,
+				Children:    childCompiledRules,
+			})
+		}
+	}
+
+	return compiledRules, nil
+}
+
+func (e *Engine) Execute(_ Input) (Output, error) {
 	panic("not yet implemented")
 }
---
+++ rules/engine_test.go
@@ -0,0 +1,160 @@
+package rules
+
+import (
+	"github.com/antonmedv/expr"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_compileRule(t *testing.T) {
+	t.Run("returns an error if the filter compilation fails", func(t *testing.T) {
+		r := Rule{
+			Filter: "INVALID UNPARSABLE FILTER",
+		}
+
+		crs, err := compileRules([]Rule{r})
+		assert.Error(t, err)
+		assert.Nil(t, crs)
+		assert.Contains(t, err.Error(), "filter compilation:")
+	})
+
+	t.Run("returns a compiled rule", func(t *testing.T) {
+		r := Rule{
+			Description: "On Off ZCL",
+			Filter:      "0x0006 in Endpoint.InClusters",
+			Actions: Actions{
+				Capabilities: Capabilities{
+					Add: map[string]interface{}{
+						"ZclOnOff": nil,
+					},
+				},
+			},
+		}
+
+		cr, err := compileRules([]Rule{r})
+		assert.NoError(t, err)
+
+		assert.Equal(t, r.Description, cr[0].Description)
+		assert.NotNil(t, cr[0].Filter)
+		assert.Equal(t, r.Actions, cr[0].Actions)
+		assert.Nil(t, r.Children)
+	})
+}
+
+func TestEngine_CompileRules(t *testing.T) {
+	t.Run("raises an error if a depended on ruleset is not loaded", func(t *testing.T) {
+		e := Engine{
+			RuleSets: map[string]RuleSet{
+				"one": {
+					Name:      "one",
+					DependsOn: []string{"two"},
+				},
+			},
+		}
+
+		err := e.CompileRules()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "ruleset missing dependency: one->two")
+	})
+
+	t.Run("raises an error if there is a circular dependency", func(t *testing.T) {
+		e := Engine{
+			RuleSets: map[string]RuleSet{
+				"one": {
+					Name:      "one",
+					DependsOn: []string{"two"},
+				},
+				"two": {
+					Name:      "two",
+					DependsOn: []string{"one"},
+				},
+			},
+		}
+
+		err := e.CompileRules()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "ruleset circular dependency: one->two->one")
+	})
+
+	t.Run("raises an error if a rule fails to compile", func(t *testing.T) {
+		e := Engine{
+			RuleSets: map[string]RuleSet{
+				"one": {
+					Name: "one",
+					Rules: []Rule{
+						{
+							Description: "this rule",
+							Filter:      "INVALID UNPARSABLE FILTER",
+						},
+					},
+				},
+			},
+		}
+
+		err := e.CompileRules()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "ruleset compilation: one: filter compilation:")
+	})
+
+	t.Run("successfully compiles nested rules and resolves execution order", func(t *testing.T) {
+		e := Engine{
+			RuleSets: map[string]RuleSet{
+				"one": {
+					Name:      "one",
+					DependsOn: []string{"two"},
+					Rules: []Rule{
+						{
+							Description: "one",
+							Filter:      "1 == 1",
+						},
+						{
+							Description: "two",
+							Filter:      "1 == 1",
+							Children: []Rule{
+								{
+									Description: "two-one",
+									Filter:      "1 == 1",
+								},
+							},
+						},
+					},
+				},
+				"two": {
+					Name: "two",
+					Rules: []Rule{
+						{
+							Description: "three",
+							Filter:      "1 == 1",
+						},
+					},
+				},
+			},
+		}
+
+		vm, _ := expr.Compile("1 == 1", expr.Env(Input{}))
+
+		expectedRules := []CompiledRule{
+			{
+				Description: "three",
+				Filter:      vm,
+			},
+			{
+				Description: "one",
+				Filter:      vm,
+			},
+			{
+				Description: "two",
+				Filter:      vm,
+				Children: []CompiledRule{
+					{
+						Description: "two-one",
+						Filter:      vm,
+					},
+				},
+			},
+		}
+
+		assert.NoError(t, e.CompileRules())
+		assert.Equal(t, expectedRules, e.Rules)
+	})
+}

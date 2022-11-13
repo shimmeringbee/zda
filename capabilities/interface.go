a8eb29b21df1059b3c87784064f813c982e4db3b
Peter <peter@alastria.net>
2022-11-13 19:28:47 +0000

    Tweak interface for capability attachment, remove endpoint and allow specification via data blob.

--- capabilities/interface.go
+++ capabilities/interface.go
@@ -3,11 +3,16 @@ package catabilities
 import (
 	"context"
 	"github.com/shimmeringbee/da"
-	"github.com/shimmeringbee/zigbee"
+)
+
+const (
+	DataKeyAlreadyInitialised = "Initialised"
+	DataKeyZigbeeEndpoint     = "ZigbeeEndpoint"
+	DataKeyZigbeeClusterID    = "ZigbeeClusterID"
 )
 
 type ZDACapability interface {
-	Attach(context.Context, da.Device, da.Gateway, zigbee.Endpoint, map[string]interface{}) (bool, error)
+	Attach(context.Context, da.Device, da.Gateway, map[string]interface{}) (bool, error)
 	Detach(context.Context) error
 	State(context.Context) (map[string]interface{}, error)
 }
